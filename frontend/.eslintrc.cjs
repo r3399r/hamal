176803fe0cbfd21686fc06a227fcca3ffb7490d7
Yue <yue@potentia.asia>
2023-11-23 14:05:43 +0800

    tailwindcss introduce

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -3,11 +3,15 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+  env: {
+    node: true
+  },
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    "plugin:tailwindcss/recommended"
   ],
   parserOptions: {
     ecmaVersion: 'latest'
---
+++ frontend/src/views/CalculatorView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div className="flex gap-1 m-1">
+    <div class="w-1/2">
+      <div class="bg-blue-400 p-2 text-white">攻方</div>
+    </div>
+    <div class="w-1/2">
+      <div class="bg-red-400 p-2 text-white">守方</div>
+    </div>
+  </div>
+</template>
